b411f6629f2fa2ecf5e47a0562d109b87222b6c3
Eremey Malinovsky <Elerphore@gmail.com>
2022-12-01 11:06:10 +0500

    *

--- college/magtu-parser-ed/src/main/kotlin/data/GroupItem.kt
+++ college/magtu-parser-ed/src/main/kotlin/data/GroupItem.kt
@@ -1,9 +1,18 @@
 package data
 
+import com.fasterxml.jackson.annotation.JsonAlias
+
 data class GroupItem(
     val name: String? = null,
+    @JsonAlias("count")
     val countItem: CountItem? = CountItem(),
+
+    @JsonAlias("holidaySection")
     val holidaySectionItem: HolidaySectionItem? = HolidaySectionItem(),
+
+    @JsonAlias("comeSection")
     val comeSectionItem: ComeSectionItem? = ComeSectionItem(),
+
+    @JsonAlias("excludeSection")
     val excludeSectionItem: ExcludeSectionItem? = ExcludeSectionItem()
 )
